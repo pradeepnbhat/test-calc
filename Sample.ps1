769f559187b7c46fc3f1b0cb72bd5703b4788876
Pradeep Narayan <pradeepn@microsoft.com>
2018-03-16 17:31:28 +0000

    Added 3 files to /

---
+++ Sample.ps1
@@ -0,0 +1,5 @@
+while(!(test-path d:\test.txt))
+{
+    write-host "Release: d:\test.txt File not exist yet. File not exist yet.File not exist yet.File not exist yet.File not exist yet."; 
+    start-sleep 2
+}
